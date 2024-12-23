5a10bf93096f945182c053f24cd705d3b5c7b0c1
Margaret C <cezarlofellem@gmail.com>
2024-12-23 17:47:03 -0500

    Added indexjs codes

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,15 @@
 function generatePoem(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let poemElement = document.querySelector("#poem");
-    poemElement.innerHTML =
-      "La courbe de tes yeux fait le tour de mon cœur, Un rond de danse et de douceur, Auréole du temps, berceau nocturne et sûr, Et si je ne sais plus tout ce que j’ai vécu C’est que t";
+  new Typewriter("#typewriter", {
+    strings: [
+      "La courbe de tes yeux fait le tour de mon cœur, Un rond de danse et de douceur, Auréole du temps, berceau nocturne et sûr, Et si je ne sais plus tout ce que j’ai vécu C’est que t",
+    ],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 
-
-let poemFormElement = document.querySelector("form");
+let poemFormElement = document.querySelector("poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
