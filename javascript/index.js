3e9e354b8bed71120fbbe29243daf102866588a9
Margaret C <cezarlofellem@gmail.com>
2024-12-23 19:47:41 -0500

    Added API Integration

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#typewriter", {
-    strings: [
-      "La courbe de tes yeux fait le tour de mon cœur, Un rond de danse et de douceur, Auréole du temps, berceau nocturne et sûr, Et si je ne sais plus tout ce que j’ai vécu C’est que t",
-    ],
+    strings: [`${displayPoem}`],
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
 
-let poemFormElement = document.querySelector("poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "39284b94tea8fb240cc5d3o3a355d5a0";
+  let prompt = `Generate a French poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic poem expert. Who loves to create French poem.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${prompt}`);
+
+  let poemFormElement = document.querySelector("poem-generator-form");
+  poemFormElement.addEventListener("submit", generatePoem);
+}
