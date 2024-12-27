e5fa5c4b691066a5446d02fd9a61ed80bb72f55c
Margaret C <cezarlofellem@gmail.com>
2024-12-27 15:00:07 -0500

    Added animation

--- javascript/index.js
+++ javascript/index.js
@@ -12,16 +12,20 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "39284b94tea8fb240cc5d3o3a355d5a0";
-  let prompt = `Generate a French poem about ${instructionsInput.value}`;
+  let prompt = `<div class="blink">Generate a French poem about ${instructionsInput.value}</div>`;
   let context =
     "You are a romantic poem expert. Who loves to create French poem.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  axios.get(apiURL).then(displayPoem);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">Generating a French poem ${instructionsInputValue}</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${prompt}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
